b36575d6e39c113d35a5c9cb06487f65a118eabb
17854030310 <95289499+17854030310@users.noreply.github.com>
2023-05-17 16:36:00 +0800

    前端资料
    前端资料

---
+++ 前端/sg-vue-admin/src/views/dashboard/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      博客管理系统
+    </div>
+    <div>
+      欢迎访问IT博客系统
+    </div>
+    <div>
+     QQ：2218722664
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
